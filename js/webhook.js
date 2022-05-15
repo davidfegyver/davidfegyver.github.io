b6f8d30f0f8f1ef13d180c8533807ffd2c9243c8
David Fegyver <fegyverdavid.bator@gmail.com>
2022-05-15 16:10:00 +0200

    🔒 Secured the contact form :)

--- js/webhook.js
+++ js/webhook.js
@@ -4,41 +4,19 @@ document.getElementById("send").onclick = (e) => {
   //Please don't hack me :)
 
   fetch(
-    atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvOTc0NzI5OTU3OTY2MjM3NzM2LzZ4VGdNV18wNFItUXpKRXlKQ1hERF9ZYndadEI3bW43OHBTTnZ4dVJiTUx4QUloVDFvU2NRMkR4cXM5N2MzUm5KOWhO'),
+    "http://davidf.tk:3000/contact",
     {
       method: "POST",
       headers: {
-        Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        content: null,
-        embeds: [
-          {
-            color: null,
-            fields: [
-              {
-                name: "Email",
-                value: email,
-              },
-              {
-                name: "Message",
-                value: message,
-              },
-            ],
-          },
-        ],
-        username: "You received a message",
-      }),
+      body: JSON.stringify({email,message}),
     }
   )
     .then(
-      (_) =>
-        (document.getElementById("response").innerText =
-          "You succesfully sent your message")
+      (response) => {
+        document.getElementById("response").innerHTML = response.body
+      
+      }
     )
-    .catch(
-      (_) =>
-        (document.getElementById("error").innerText = "There was an error :/")
-    );
 };
